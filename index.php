e21acbe4bfb224fee4c78f7437b7317a1f8bdafa
izerol2k2 <75089417+izerol2k2@users.noreply.github.com>
2020-11-27 00:18:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html><head>
+
+<title>Repo by TQM-Mos (Minh Hi)</title>
+
+<meta name='viewport' content='width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;'/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+  <link href="JS/styles.css" rel="stylesheet">
+  <script src="JS/jquery-3.2.1.min.js"></script>
+  <script src="JS/description.js?_=4654654"></script>
+  
+</head>
+<body>
+				   <p style="text-align:center;"><IMG height=110 width=320 border=1 src="Mos/logo.png" > </A>
+<p style="text-align:center;"><u><mark><strong><MARQUEE behavior=alternate direction=left scrollAmount=3 width="4%"><font face=Webdings></font></MARQUEE><MARQUEE scrollAmount=1 direction=left width="2%">| | |</MARQUEE>Minh Hí ™︵⁹² <MARQUEE scrollAmount=1 direction=right width="2%">| | |</MARQUEE><MARQUEE behavior=alternate direction=right scrollAmount=3 width="4%"><font face=Webdings></font></MARQUEE>
+		</div></a>
+	</fieldset>
+	<fieldset>
+		<a class="btn btn-sm btn-default" href="cydia://url/https://cydia.saurik.com/api/share#?source=http://tqm-mos.github.io/">
+				    <p style="text-align:center;"><img class="icon" src="Mos/cydia.png"/>
+<p style="text-align:center;"><mark><strong><marquee behavior="alternate" width="10%"></marquee>Add Source Cydia<marquee behavior="alternate" width="10%"></marquee>
+		<a href="sileo://source/https://tqm-mos.github.io/">
+					<p style="text-align:center;"><img class="icon" src="Mos/sileo.png"/>
+<p style="text-align:center;"><mark><strong><marquee behavior="alternate" width="10%"></marquee>Add Source Sileo<marquee behavior="alternate" width="10%"></marquee>
+		</div></a>
+	</fieldset>
+	<fieldset>
+<p style="text-align:center;"><strong>Anh em nào trong Sài Gòn qua Quận 2 giao lưu 😂</strong></p>	
+</div></a>
+	</fieldset>
+	<fieldset>
+	</body>
+</html>
